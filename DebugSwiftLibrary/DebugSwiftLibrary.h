528fa3a172aee27fe80db3bc820dcfe3e2a38190
Vasyl <vasya6868@gmail.com>
2020-10-03 00:15:35 +0300

    Commit

---
+++ DebugSwiftLibrary/DebugSwiftLibrary.h
@@ -0,0 +1,18 @@
+//
+//  DebugSwiftLibrary.h
+//  DebugSwiftLibrary
+//
+//  Created by Vasyl Seredniy on 02.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DebugSwiftLibrary.
+FOUNDATION_EXPORT double DebugSwiftLibraryVersionNumber;
+
+//! Project version string for DebugSwiftLibrary.
+FOUNDATION_EXPORT const unsigned char DebugSwiftLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebugSwiftLibrary/PublicHeader.h>
+
+
